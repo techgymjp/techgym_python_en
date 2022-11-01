6c235d69ca295f377c4362974f369b48940bac74
tomi118 <tomikaruge@gmail.com>
2022-11-01 12:19:22 +0200

    fixed

--- dV9E.py
+++ dV9E.py
@@ -2,35 +2,42 @@ import random
 
 hands = ['rock', 'scissors', 'paper']
 
+
 def start_message():
-  print('Start \'rock-scissors-paper\'')
+    print('Start \'rock-scissors-paper\'')
 
-def get_my_hand():
-  print('Input your hand')
-  return int(input('0:rock, 1:scissors, 2:paper'))
 
-def get_you_hand():
-  return random.randint(0, 2)
+def get_player():
+    print('Input your hand')
+    return int(input('0:rock, 1:scissors, 2:paper'))
+
+
+def get_computer():
+    return random.randint(0, 2)
+
 
 def get_hand_name(hand_number):
-  return hands[hand_number]
+    return hands[hand_number]
+
 
 def view_hand(my_hand, you_hand):
-  print('My hand is ' + get_hand_name(my_hand))
-  print('Rival\'s hand is ' + get_hand_name(you_hand))
+    print('My hand is ' + get_hand_name(my_hand))
+    print('Rival\'s hand is ' + get_hand_name(you_hand))
+
 
 def view_result(hand_diff):
-  if hand_diff == 0:
-    print('draw')
-  elif hand_diff == -1 or hand_diff == 2:
-    print('win')
-  else:
-    print('lose')
+    if hand_diff == 0:
+        print('draw')
+    elif hand_diff == -1 or hand_diff == 2:
+        print('win')
+    else:
+        print('lose')
+
 
 start_message()
 
-my_hand = get_my_hand()
-you_hand = get_you_hand()
+my_hand = get_player()
+you_hand = get_computer()
 hand_diff = my_hand - you_hand
 
 view_hand(my_hand, you_hand)
--- gP6s.py
+++ gP6s.py
@@ -1,27 +1,32 @@
 import random
 
+
 def start_message():
-  print('Start \'rock-scissors-paper\'')
+    print('Start \'rock-scissors-paper\'')
 
-def get_my_hand():
-  print('Input your hand')
-  return int(input('0:rock, 1:scissors, 2:paper'))
 
-def get_you_hand():
-  return random.randint(0, 2)
+def get_player():
+    print('Input your hand')
+    return int(input('0:rock, 1:scissors, 2:paper'))
+
+
+def get_computer():
+    return random.randint(0, 2)
+
 
 def view_result(hand_diff):
-  if hand_diff == 0:
-    print('draw')
-  elif hand_diff == -1 or hand_diff == 2:
-    print('win')
-  else:
-    print('lose')
+    if hand_diff == 0:
+        print('draw')
+    elif hand_diff == -1 or hand_diff == 2:
+        print('win')
+    else:
+        print('lose')
+
 
 start_message()
 
-my_hand = get_my_hand()
-you_hand = get_you_hand()
+my_hand = get_player()
+you_hand = get_computer()
 hand_diff = my_hand - you_hand
 
 view_result(hand_diff)
