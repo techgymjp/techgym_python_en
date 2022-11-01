ae9c93cf4ff29c197ff0b9443543a1ea9c3d5011
tomi118 <tomikaruge@gmail.com>
2022-11-01 12:26:04 +0200

    fixed function name

--- Ef3z.py
+++ Ef3z.py
@@ -1,62 +1,72 @@
 import random
 
 hands = ['rock', 'scissors', 'paper']
-results = {'win':'win', 'lose':'lose', 'draw':'draw'}
+results = {'win': 'win', 'lose': 'lose', 'draw': 'draw'}
+
 
 def start_message():
-  print('Start \'rock-scissors-paper\'')
+    print('Start \'rock-scissors-paper\'')
+
 
 def is_hand(number):
-  if number >= 0 and number <= 2:
-    return True
-  else:
-    return False
+    if number >= 0 and number <= 2:
+        return True
+    else:
+        return False
 
-def get_my_hand():
-  print('Input your hand')
-  input_message = ''
-  index = 0
-  for hand in hands:
-    input_message += str(index) + ':' + hand
-    if index < 2:
-      input_message += ', '
-    index += 1
-  return int(input(input_message))
 
-def get_you_hand():
-  return random.randint(0, 2)
+def get_player():
+    print('Input your hand')
+    input_message = ''
+    index = 0
+    for hand in hands:
+        input_message += str(index) + ':' + hand
+        if index < 2:
+            input_message += ', '
+        index += 1
+    return int(input(input_message))
+
+
+def get_computer():
+    return random.randint(0, 2)
+
 
 def get_hand_name(hand_number):
-  return hands[hand_number]
+    return hands[hand_number]
+
 
 def view_hand(my_hand, you_hand):
-  print('My hand is ' + get_hand_name(my_hand))
-  print('Rival\'s hand is ' + get_hand_name(you_hand))
+    print('My hand is ' + get_hand_name(my_hand))
+    print('Rival\'s hand is ' + get_hand_name(you_hand))
+
 
 def get_result(hand_diff):
-  if hand_diff == 0:
-    return 'draw'
-  elif hand_diff == -1 or hand_diff == 2:
-    return 'win'
-  else:
-    return 'lose'
+    if hand_diff == 0:
+        return 'draw'
+    elif hand_diff == -1 or hand_diff == 2:
+        return 'win'
+    else:
+        return 'lose'
+
 
 def view_result(result):
-  print(results[result])
+    print(results[result])
+
 
 def play():
-  my_hand = get_my_hand()
-  while not is_hand(my_hand):
-    my_hand = get_my_hand()
+    my_hand = get_player()
+    while not is_hand(my_hand):
+        my_hand = get_player()
 
-  you_hand = get_you_hand()
-  hand_diff = my_hand - you_hand
+    you_hand = get_computer()
+    hand_diff = my_hand - you_hand
 
-  view_hand(my_hand, you_hand)
-  result = get_result(hand_diff)
-  view_result(result)
-  if result == 'draw':
-    play()
+    view_hand(my_hand, you_hand)
+    result = get_result(hand_diff)
+    view_result(result)
+    if result == 'draw':
+        play()
+
 
 start_message()
 play()
