6ab4410a135533fb2c0e761bae0f4dcb1f48643a
tomi118 <tomikaruge@gmail.com>
2022-11-08 16:33:21 +0200

    fixed Rf4q.py

--- Pk3H.py
+++ Pk3H.py
@@ -1,92 +1,103 @@
 import random
 import math
 
-data = [['O','0'], ['l','1'], ['u','v']]
+data = [['O', '0'], ['l', '1'], ['u', 'v']]
 number_data = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
 level = 1
 col = 3
 row = 3
 
+
 def start_message():
-  print('Input cell number (e.g. A1) of the different character.')
+    print('Input cell number (e.g. A1) of the different character.')
+
 
 def section_message():
-  print('level: ' + str(level))
+    print('level: ' + str(level))
+
 
 def view_question():
-  choice_data = random.randint(0, 2)
-  mistake_number = random.randint(0, (col * row) - 1 )
-  question = data[choice_data]
-  print(question)
-  i = 0
-  j = 0
-  question_str1 = '/|'
-  question_str2 = '--'
-  while i < col:
-    question_str1 += number_data[i] + ''
-    question_str2 += '-'
-    i += 1
-  print(question_str1)
-  print(question_str2)
-  i = 0
-  while i < row:
-    question_str = str(i + 1) + '|'
-    while j < col:
-      if (i * col + j) == mistake_number:
-        question_str += question[1]
-      else:
-        question_str += question[0]
-      j += 1
-    print(question_str)
-    i += 1
+    choice_data = random.randint(0, 2)
+    mistake_number = random.randint(0, (col * row) - 1)
+    question = data[choice_data]
+    print(question)
+    i = 0
     j = 0
-  return mistake_number
+    question_str1 = '/|'
+    question_str2 = '--'
+    while i < col:
+        question_str1 += number_data[i] + ''
+        question_str2 += '-'
+        i += 1
+    print(question_str1)
+    print(question_str2)
+    i = 0
+    while i < row:
+        question_str = str(i + 1) + '|'
+        while j < col:
+            if (i * col + j) == mistake_number:
+                question_str += question[1]
+            else:
+                question_str += question[0]
+            j += 1
+        print(question_str)
+        i += 1
+        j = 0
+    return mistake_number
+
 
 def change_input_number(input_str):
-  str_data = { 'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7, 'I':8 }
-  input_str_split = list(input_str)
-  col_number = str_data[input_str_split[0]]
-  row_number = int(input_str_split[1]) - 1
-  input_number = row_number * col + col_number
-  return input_number
+    str_data = {'A': 0, 'B': 1, 'C': 2, 'D': 3,
+                'E': 4, 'F': 5, 'G': 6, 'H': 7, 'I': 8}
+    input_str_split = list(input_str)
+    col_number = str_data[input_str_split[0]]
+    row_number = int(input_str_split[1]) - 1
+    input_number = row_number * col + col_number
+    return input_number
+
 
 def is_correct_number(mistake_number, input_number):
-  if mistake_number == input_number:
-    return True
-  else:
-    return False
+    if mistake_number == input_number:
+        return True
+    else:
+        return False
+
 
 def view_result(is_correct, mistake_number):
-  if is_correct:
-    print('Correct!')
-    if level < 13:
-      level_up()
-      play()
-  else:
-    print('Wrong')
-    print('Correct answer is ' + change_string(mistake_number))
+    if is_correct:
+        print('Correct!')
+        if level < 13:
+            level_up()
+            play()
+    else:
+        print('Wrong')
+        print('Correct answer is ' + change_string(mistake_number))
+
 
 def change_string(number):
-  col_number = number % col
-  row_number = math.floor(number / col) + 1
-  string = number_data[col_number] + str(row_number)
-  return string
+    col_number = number % col
+    row_number = math.floor(number / col) + 1
+    string = number_data[col_number] + str(row_number)
+    return string
+
 
 def level_up():
-  global level, col, row
-  level += 1
-  if level % 2 == 0:
-    col += 1
-  else:
-    row += 1
+    global level, col, row
+    level += 1
+    if level % 2 == 0:
+        col += 1
+    else:
+        row += 1
+
 
 def play():
-  section_message()
-  mistake_number = view_question()
-  choice = input('(e.g. A1)')
-  input_number = change_input_number(choice)
-  is_correct = is_correct_number(mistake_number, input_number)
-  view_result(is_correct, mistake_number)
+    section_message()
+    mistake_number = view_question()
+    choice = input('(e.g. A1)')
+    input_number = change_input_number(choice)
+    is_correct = is_correct_number(mistake_number, input_number)
+    view_result(is_correct, mistake_number)
+
 
 start_message()
 play()
--- Rf4q.py
+++ Rf4q.py
@@ -4,102 +4,111 @@ import math
 teams = []
 playing_teams = {'myself': False, 'enemy': False}
 
+
 class Team:
-  def __init__(self, id, name, attack, defense):
-    self.id = id
-    self.name = name
-    self.attack = attack
-    self.defense = defense
-    self.total_score = 0
+    def __init__(self, id, name, attack, defense):
+        self.id = id
+        self.name = name
+        self.attack = attack
+        self.defense = defense
+        self.total_score = 0
 
-  def info(self):
-    print(self.name + ': Offensive power:' + str(self.attack) + ' / Defensive power:' + str(self.defense))
+    def info(self):
+        print(self.name + ': Offensive power:' + str(self.attack) +
+              ' / Defensive power:' + str(self.defense))
 
-  def get_hit_rate(self):
-    return random.randint(10, self.attack)
+    def get_hit_rate(self):
+        return random.randint(10, self.attack)
 
-  def get_out_rate(self):
-    return random.randint(10, self.defense)
+    def get_out_rate(self):
+        return random.randint(10, self.defense)
+
 
 def create_teams():
-  global teams
-  team1 = Team('Atackers', 80, 20)
-  team2 = Team('Defenders', 30, 70)
-  team3 = Team('Averages', 50, 50)
-  teams = [team1, team2, team3]
+    global teams
+    team1 = Team(1, 'Atackers', 80, 20)
+    team2 = Team(2, 'Defenders', 30, 70)
+    team3 = Team(3, 'Averages', 50, 50)
+    teams = [team1, team2, team3]
+
 
 def show_teams():
-  index = 1
-  print('Information of all teams')
-  for team in teams:
-    print(str(index))
-    team.info()
-    index += 1
+    index = 1
+    print('Information of all teams')
+    for team in teams:
+        print(str(index))
+        team.info()
+        index += 1
+
 
 def choice_team(player):
-  if player == 'myself':
-    player_name = 'Your'
-  elif player == 'enemy':
-    player_name = 'Opponent\'s'
+    if player == 'myself':
+        player_name = 'Your'
+    elif player == 'enemy':
+        player_name = 'Opponent\'s'
 
-  choice_team_number = int(input('Select ' + player_name + ' team(1-3) '))
-  choice_team_number = input(choice_team_message)
-  while not enable_choice_team(choice_team_number, player):
+    choice_team_message = 'Select ' + player_name + ' team(1-3) '
     choice_team_number = input(choice_team_message)
-  choice_team_number = int(choice_team_number)
+    while not enable_choice_team(choice_team_number, player):
+        choice_team_number = input(choice_team_message)
+    choice_team_number = int(choice_team_number)
 
-  playing_teams[player] = teams[choice_team_number - 1]
-  print(player_name + ' team is \'' + playing_teams[player].name + '\'')
+    playing_teams[player] = teams[choice_team_number - 1]
+    print(player_name + ' team is \'' + playing_teams[player].name + '\'')
+
 
 def enable_choice_team(string, player):
-  if string.isdigit():
-    number = int(string)
-    if number >= 1 and number <= 3:
-      if player == 'enemy':
-        if number == playing_teams['myself'].id:
-          return False
-      return True
+    if string.isdigit():
+        number = int(string)
+        if number >= 1 and number <= 3:
+            if player == 'enemy':
+                if number == playing_teams['myself'].id:
+                    return False
+            return True
+        else:
+            return False
     else:
-      return False
-  else:
-    return False
+        return False
+
 
 def get_play_inning(inning):
-  if inning == 'front':
-    hit_rate = playing_teams['myself'].get_hit_rate()
-    out_rate = playing_teams['enemy'].get_out_rate()
-  elif inning == 'back':
-    hit_rate = playing_teams['enemy'].get_hit_rate()
-    out_rate = playing_teams['myself'].get_out_rate()
-  inning_score = math.floor((hit_rate - out_rate) / 10)
-  if inning_score < 0:
-    inning_score = 0
-  return inning_score
+    if inning == 'top':
+        hit_rate = playing_teams['myself'].get_hit_rate()
+        out_rate = playing_teams['enemy'].get_out_rate()
+    elif inning == 'bottom':
+        hit_rate = playing_teams['enemy'].get_hit_rate()
+        out_rate = playing_teams['myself'].get_out_rate()
+    inning_score = math.floor((hit_rate - out_rate) / 10)
+    if inning_score < 0:
+        inning_score = 0
+    return inning_score
+
 
 def play():
-  create_teams()
-  show_teams()
-  choice_team('myself')
-  choice_team('enemy')
-  score_boards = ['________|', 'You     |','Opponent|']
-  for i in range(9):
-    score_boards[0] += str(i + 1) + '｜'
-    # 表の攻撃
-    inning_score = get_play_inning('front')
-    score_boards[1] += str(inning_score) + '｜'
-    playing_teams['myself'].total_score += inning_score
-    # 裏の攻撃
-    if i == 8 and playing_teams['myself'].total_score < playing_teams['enemy'].total_score:
-      score_boards[2] += 'X｜'
-    else:
-      inning_score = get_play_inning('back')
-      score_boards[2] += str(inning_score) + '｜'
-      playing_teams['enemy'].total_score += inning_score
-  score_boards[0] += 'R｜'
-  score_boards[1] += str(playing_teams['myself'].total_score) + '｜'
-  score_boards[2] += str(playing_teams['enemy'].total_score) + '｜'
-  print(score_boards[0])
-  print(score_boards[1])
-  print(score_boards[2])
+    create_teams()
+    show_teams()
+    choice_team('myself')
+    choice_team('enemy')
+    score_boards = ['________|', 'You     |', 'Opponent|']
+    for i in range(9):
+        score_boards[0] += str(i + 1) + '｜'
+        # attack of the top
+        inning_score = get_play_inning('top')
+        score_boards[1] += str(inning_score) + '｜'
+        playing_teams['myself'].total_score += inning_score
+        # attack of the bottom
+        if i == 8 and playing_teams['myself'].total_score < playing_teams['enemy'].total_score:
+            score_boards[2] += 'X｜'
+        else:
+            inning_score = get_play_inning('bottom')
+            score_boards[2] += str(inning_score) + '｜'
+            playing_teams['enemy'].total_score += inning_score
+    score_boards[0] += 'R｜'
+    score_boards[1] += str(playing_teams['myself'].total_score) + '｜'
+    score_boards[2] += str(playing_teams['enemy'].total_score) + '｜'
+    print(score_boards[0])
+    print(score_boards[1])
+    print(score_boards[2])
+
 
 play()
